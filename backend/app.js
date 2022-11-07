3bebb79f463f760b030b2252ab61fb474d29705b
hilminomad <hilmiansari@gmail.com>
2022-11-07 10:26:06 +0100

    login functionality changes

--- backend/app.js
+++ backend/app.js
@@ -12,11 +12,11 @@ const app = express();
 
 const connectDB = require('./db/connect');
 
-//const authentificateUser = require('./middleware/authentication');
+const authentificateUser = require('./middleware/authentication');
 
 // routers
 
-//const authRouter = require('./routes/auth');
+const authRouter = require('./routes/auth');
 const productsRouter = require('./routes/products');
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
@@ -38,7 +38,7 @@ app.use(express.json());
 //app.use(cors);
 //app.use(xss);
 // routes
-//app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/auth', authRouter);
 
 app.use('/api/v1/products', /*authentificateUser,*/ productsRouter);
 
--- frontend/src/pages/login/login.js
+++ frontend/src/pages/login/login.js
@@ -1,41 +1,75 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!email || !password) return;
+    const user = { email, password };
+
+    try {
+      const url = `${rootUrl}/api/v1/auth/login`;
+      // const url = `/api/v1/auth/login`;
+      await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(user),
+      });
+      setPassword('');
+      setEmail('');
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <section className="hero login" style={{ backgroundImage: "none" }}>
-      <div className="login__container">
-        <div className="login__signin">
+    <section className='hero login' style={{ backgroundImage: 'none' }}>
+      <div className='login__container'>
+        <div className='login__signin'>
           <h2>
             Sign in to your<br></br>Account
           </h2>
-          <form action="submit" className="footer__form login__signin_form">
-            <input type="email" placeholder="Email" />
+          <form
+            action='submit'
+            onSubmit={hundleSubmit}
+            className='footer__form login__signin_form'
+          >
             <input
-              type="password"
-              placeholder="Password"
-              placeholderTextColor="#806e28"
+              type='email'
+              placeholder='Email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
-            <div className="login__signin_submitbutton">
-              <button type="submit">
+            <input
+              type='password'
+              placeholder='Password'
+              placeholderTextColor='#806e28'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <div className='login__signin_submitbutton'>
+              <button type='submit'>
                 <p>SIGN IN</p>
               </button>
             </div>
-            <div className="login__signin_forgotpasswordbutton">
+            <div className='login__signin_forgotpasswordbutton'>
               <button>
-                <a href="/">FORGOT PASSWORD &gt;</a>
+                <a href='/'>FORGOT PASSWORD &gt;</a>
               </button>
             </div>
           </form>
         </div>
-        <div className="login__seperator"></div>
-        <div className="login__signin">
+        <div className='login__seperator'></div>
+        <div className='login__signin'>
           <h2>
             New<br></br>Costomer
           </h2>
-          <div className="login__signin_submitbutton">
-            <button type="submit">
-              <Link to={"/create"}>
+          <div className='login__signin_submitbutton'>
+            <button type='submit'>
+              <Link to={'/create'}>
                 <p>CREATE ACCOUNT</p>
               </Link>
             </button>
